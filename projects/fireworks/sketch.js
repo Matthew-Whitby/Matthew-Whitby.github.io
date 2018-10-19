cb8f56b4363309a7ab1663c7eb551ce1414d21d9
Whitbyfish <34402877+Whitbyfish@users.noreply.github.com>
2018-10-19 20:33:17 +0100

    a

--- projects/fireworks/sketch.js
+++ projects/fireworks/sketch.js
@@ -3,7 +3,6 @@ var gravity;
 function setup(){
   createCanvas(400,300);
   gravity = createVector(0,0.2);
-  fireworks.push(new Firework());
   stroke(255);
   strokeWeight(4);
 
@@ -11,6 +10,7 @@ function setup(){
 
 function draw() {
   background(51);
+  fireworks.push(new Firework());
   for(var i = 0;i < fireworks.length; i++){
     fireworks[i].update();
     fireworks[i].show();

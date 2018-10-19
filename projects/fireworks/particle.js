07155ef46bb6464c52009d36c93ef8dad7144db9
Whitbyfish <34402877+Whitbyfish@users.noreply.github.com>
2018-10-19 21:40:11 +0100

    a

--- projects/fireworks/particle.js
+++ projects/fireworks/particle.js
@@ -6,7 +6,7 @@ function Particle(x,y,firework){
     this.vel = createVector(0, random(-7,-10)); //Upwards velocity 2p
   } else {
     this.vel = p5.Vector.random2D();
-    this.vel.mult(random(1,6)); //magnitude of explosion
+    this.vel.mult(random(2,10)); //magnitude of explosion
   }
 
   this.acc = createVector(0,0);
@@ -23,6 +23,14 @@ this.applyForce = function(force) {
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.mult(0);
+  }
+
+  this.done(){
+    if (this.lifespan < 0){
+      return true;
+    }else{
+      return false;
+    }
   }
 
   this.show = function(){

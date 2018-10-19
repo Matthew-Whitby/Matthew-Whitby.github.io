11cc8c0a6b54f942dee2ed380f401df34735b15f
Whitbyfish <34402877+Whitbyfish@users.noreply.github.com>
2018-10-19 20:55:43 +0100

    a

--- projects/fireworks/firework.js
+++ projects/fireworks/firework.js
@@ -1,6 +1,7 @@
 function Firework(){
   this.firework = new Particle(random(width),height);
   this.exploded = false;
+  this.particles = [];
 
   this.update = function() {
     if(!this.exploded){
@@ -8,13 +9,24 @@ function Firework(){
       this.firework.update();
       if(this.firework.vel.y >= 0){
         this.exploded = true;
+        this.explode();
       }
+    }
+  }
+
+  this.explode = function(){
+    for (var i = 0; i < 100; i++){
+      var p = new Particle(this.firework.pos.x,this.firework.pos.y);
+      this.particles.push(p);
     }
   }
 
   this.show = function(){
     if(!this.exploded){
       this.firework.show();
+    }
+    for(var i = 0; i < this.particles.length; i++){
+      this.particles[i].show();
     }
   }
 }

8cf2e4135f8cc0d215b359530865fe8b532ab523
Whitbyfish <34402877+Whitbyfish@users.noreply.github.com>
2018-10-20 18:03:39 +0100

    a

--- projects/snowfall/sketch.js
+++ projects/snowfall/sketch.js
@@ -33,13 +33,13 @@ function draw(){
   background(0);
   //image(textures,0,0);
   //snow.push(new Snowflake());
-  zOff += 0.1;
+  zOff += 0.05;
   for (flake of snow) {
     let xOff = flake.pos.x / width;
     let yOff = flake.pos.y / height;
     let wAngle = noise(xOff,yOff,zOff) * TWO_PI;
     let wind = p5.Vector.fromAngle(wAngle);
-    wind.mult(1);
+    wind.mult(0.1);
 
     flake.applyForce(gravity);
     flake.applyForce(wind);

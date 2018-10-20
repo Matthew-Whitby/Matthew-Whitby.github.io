9ec0ec96378ca2f226cfcbc1523acf88a993f8fc
Whitbyfish <34402877+Whitbyfish@users.noreply.github.com>
2018-10-20 17:13:00 +0100

    a

--- projects/fireworks/particle.js
+++ projects/fireworks/particle.js
@@ -4,7 +4,7 @@ function Particle(x,y,hu,firework){
   this.lifespan = 255;
   this.hu = hu
   if(this.firework){
-    this.vel = createVector(0, random(-7,((windowHeight / 10)/4)*-1)); //Upwards velocity 2p before -10
+    this.vel = createVector(0, random(-7,(windowHeight / 20)*-1)); //Upwards velocity 2p before -10
   } else {
     this.vel = p5.Vector.random2D();
     this.vel.mult(random(2,10)); //magnitude of explosion

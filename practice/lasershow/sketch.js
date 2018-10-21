0cb4fa7e9877ba8673331c364e29ea9d8adb1977
Whitbyfish <34402877+Whitbyfish@users.noreply.github.com>
2018-10-21 17:25:19 +0100

    a

--- practice/lasershow/sketch.js
+++ practice/lasershow/sketch.js
@@ -11,7 +11,7 @@ function setup(){
 function draw() {
   if(random(1) < 0.1){
     if(lasers.length > 0){
-      lasers.splice(lasers[0]);
+      lasers.splice(lasers[0],1);
     }
     let sx;
     let ex;

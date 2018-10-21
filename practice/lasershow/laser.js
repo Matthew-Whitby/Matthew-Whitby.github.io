3ccff7833e2d2acef663392ed38adcd3775033b6
Whitbyfish <34402877+Whitbyfish@users.noreply.github.com>
2018-10-21 16:11:59 +0100

    a

--- practice/lasershow/laser.js
+++ practice/lasershow/laser.js
@@ -11,7 +11,7 @@ class Laser {
   render() {
     stroke(255);
     strokeWeight(10);
-    if(ex == -100){
+    if(this.ex == -100){
       line(0,this.sy,windowWidth,this.ey);
     }else{
       line(this.sx,0,this.ex,windowHeight);

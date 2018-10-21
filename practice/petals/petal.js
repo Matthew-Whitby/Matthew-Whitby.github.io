8a058e7df5492d2fc2d497ba515d2e099ebf70c4
Whitbyfish <34402877+Whitbyfish@users.noreply.github.com>
2018-10-21 15:15:38 +0100

    a

--- practice/petals/petal.js
+++ practice/petals/petal.js
@@ -4,7 +4,6 @@ function getRandomSize(){
 }
 
 class Petal {
-
   constructor(sx,sy,img) {
     let x = sx || random(width); //gets initial random x or generates a random x if no inital is given
     let y = sy || random(-100,-10); //gets inital random y or generates a random y somewhere above the top of the screen so petals fall from differnt heights
@@ -15,7 +14,6 @@ class Petal {
     this.angle = random(TWO_PI); //randomizes the starting angle of petal
     this.dir = (random(1 > 0.5) ? 1 : -1); //randomizes whether petal will spin clockwise or anti-clockwise
     this.xOff = 0; //x offput of petal
-
     this.r = getRandomSize(); //chooses a random size for the petal
   }
 
@@ -36,10 +34,10 @@ class Petal {
   }
 
   update() {
-    this.xOff = sin(this.angle) * 2 * this.r;
+    this.xOff = sin(this.angle) * 2 * this.r; //uses sin wave to make petals move left and right
 
     this.vel.add(this.acc);
-    this.vel.limit(this.r * 0.2);
+    this.vel.limit(this.r * 0.2); //stops petals going too fast
 
     if(this.vel.mag() < 1){
       this.vel.normalize();
@@ -55,6 +53,7 @@ class Petal {
     if(this.pos.x < -this.r){ //wrapping left and right
       this.pos.x = width + this.r;
     }
+
     if(this.pos.x > width + this.r){
       this.pos.x = -this.r;
     }
@@ -64,13 +63,11 @@ class Petal {
 
   render() {
     push();
-    translate(this.pos.x + this.xOff,this.pos.y);
-    rotate(this.angle);
+    translate(this.pos.x + this.xOff,this.pos.y); //moves image left/right
+    rotate(this.angle); //rotates image
     imageMode(CENTER);
-    image(this.img,0,0,this.r,this.r);
+    image(this.img,0,0,this.r,this.r); //moves image
     pop();
-
-
   }
 
   offScreen(){

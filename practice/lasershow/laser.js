8e88be8af78b64dc6da1dc9a04647486d5983893
Whitbyfish <34402877+Whitbyfish@users.noreply.github.com>
2018-10-21 17:09:32 +0100

    a

--- practice/lasershow/laser.js
+++ practice/lasershow/laser.js
@@ -15,10 +15,10 @@ class Laser {
     strokeWeight(5);
     if(this.ex == -100){
       //line(0,this.sy,windowWidth,this.ey);
-      glow(0,this.sy,windowWidth,this.ey);
+      this.glow(0,this.sy,windowWidth,this.ey);
     }else{
     //  line(this.sx,0,this.ex,windowHeight);
-      glow(this.sx,0,this.ex,windowHeight);
+      this.glow(this.sx,0,this.ex,windowHeight);
     }
     pop();
   }

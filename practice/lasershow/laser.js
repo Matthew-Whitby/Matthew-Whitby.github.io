bec877fc4ec106e5df8cf881d951045712157940
Whitbyfish <34402877+Whitbyfish@users.noreply.github.com>
2018-10-21 16:16:08 +0100

    a

--- practice/lasershow/laser.js
+++ practice/lasershow/laser.js
@@ -5,16 +5,19 @@ class Laser {
     let ex = etx;
     let sy = sly;
     let ey = ery;
-
+console.log(sx);
+console.log(ex);
+console.log(sy);
+console.log(ey);
   }
 
   render() {
     stroke(255);
     strokeWeight(10);
     if(this.ex == -100){
-      line(0,this.sy,windowWidth,this.ey);
+      //line(0,this.sy,windowWidth,this.ey);
     }else{
-      line(this.sx,0,this.ex,windowHeight);
+      //line(this.sx,0,this.ex,windowHeight);
     }
   }
 

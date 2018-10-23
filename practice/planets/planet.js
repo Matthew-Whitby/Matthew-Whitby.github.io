04b92f4689cbef1506c8b0c33f5839530d76bacb
Whitbyfish <34402877+Whitbyfish@users.noreply.github.com>
2018-10-23 20:45:16 +0100

    a

--- practice/planets/planet.js
+++ practice/planets/planet.js
@@ -1,31 +1,31 @@
 class Planet {
-
-  Planet(m, x , y) {
-    this.acceleration = createVector(0,0);
+  Planet(m, x, y) {
+    this.acceleration = createVector(1,1);
     this.mass = m;
     this.position = createVector(x,y);
     this.velocity = createVector(0,0);
   }
+}
 
-  applyForce(force) {
-    var f = createVector(force,this.mass);
-    this.acceleration.add(f);
+  Planet.prototype.applyForce = function(force) {
+    //var f = createVector(force,this.mass);
+    this.acceleration.add(createVector(force,this.mass));
   }
 
-  update() {
+  Planet.prototype.update = function() {
     this.velocity.add(this.acceleration);
     this.position.add(this.velocity);
     this.acceleration.mult(0);
   }
 
-  display() {
+  Planet.prototype.display = function() {
     stroke(0);
     strokeWeight(2);
     fill(0,127);
     ellipse(this.position.x,this.position.y,this.mass*16,this.mass*16);
   }
 
-  checkEdges() {
+  Planet.prototype.checkEdges = function() {
     if (this.position.x > width) {
       this.position.x = width;
       this.velocity.x *= -1;
@@ -38,4 +38,3 @@ class Planet {
       this.position.y = height;
     }
   }
-}

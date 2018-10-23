34d1dc8c46f7fb8ef66e84416424cd2b0b0f84fa
Whitbyfish <34402877+Whitbyfish@users.noreply.github.com>
2018-10-23 21:35:24 +0100

    a

--- practice/planets/planet.js
+++ practice/planets/planet.js
@@ -1,15 +1,15 @@
 class Planet {
   Planet(m, x, y) {
-    this.acceleration = createVector(0,0);
+    /*this.acceleration = createVector(0,0);
     this.mass = m;
-    this.position = createVector(x,y);
-    this.velocity = createVector(0,0);
+    this.pos = createVector(x,y);
+    this.velocity = createVector(0,0);*/
   }
 }
 
   Planet.prototype.update = function() {
     this.velocity.add(this.acceleration);
-    this.position.add(this.velocity);
+    this.pos.add(this.velocity);
     this.acceleration.mult(0);
   }
 
@@ -19,29 +19,30 @@ class Planet {
   }
 
   Planet.prototype.declareGravity = function(m, x, y){
+    console.log(y);
     this.acceleration = createVector(0,0);
     this.mass = m;
-    this.position = createVector(x,y);
+    this.pos = createVector(x,y);
     this.velocity = createVector(0,0);
   }
 
-  Planet.prototype.display = function() {
+  Planet.prototype.show = function() {
     stroke(255);
-    strokeWeight(4);
-
-    ellipse(this.position.x,this.position.y,this.mass*16,this.mass*16);
+    strokeWeight(16*this.mass);
+    point(this.pos.x,this.pos.y);
+    //ellipse(this.position.x,this.position.y,this.mass*16,this.mass*16);
   }
 
   Planet.prototype.checkEdges = function() {
-    if (this.position.x > width) {
-      this.position.x = width;
+    if (this.pos.x > width) {
+      this.pos.x = width;
       this.velocity.x *= -1;
-    } else if (this.position.x < 0) {
+    } else if (this.pos.x < 0) {
       this.velocity.x *= -1;
-      this.position.x = 0;
+      this.pos.x = 0;
     }
-    if (this.position.y > height) {
+    if (this.pos.y > height) {
       this.velocity.y *= -1;
-      this.position.y = height;
+      this.pos.y = height;
     }
   }
--- practice/planets/sketch.js
+++ practice/planets/sketch.js
@@ -2,25 +2,23 @@ let planets = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0);
+  background(20);
   for (var i = 0; i < 20; i++) {
-    var planet = new Planet(random(1,4),0,0);
-    planets.push(planet);
-    planets[i].declareGravity(random(1,4),0,0);
+    planets.push(new Planet(random(1,4),200,20));
+    planets[i].declareGravity(random(1,4),200,20);
   }
 }
 
 function draw() {
-  background(0);
+  background(20);
 
   for (planet of planets) {
     var wind = createVector(0.01, 0);
     var gravity = createVector(0, 0.1*planet.mass);
-    planet.declareGravity();
     planet.applyForce(wind);
     planet.applyForce(gravity);
     planet.update();
-    planet.display();
+    planet.show();
     planet.checkEdges();
   }
 }

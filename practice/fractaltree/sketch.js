aaed89871177224f53d81fc5621aa2e3752e7511
Whitbyfish <34402877+Whitbyfish@users.noreply.github.com>
2018-10-24 19:11:44 +0100

    a

---
+++ practice/fractaltree/sketch.js
@@ -0,0 +1,39 @@
+var angle = 0;
+var slider;
+
+function setup(){
+  createCanvas(windowWidth,windowHeight);
+  slider = createSlider(0,TWO_PI,PI/4,0.01);
+  slider.position(0,0);
+  colorMode(HSB);
+}
+
+function draw(){
+    background(0);
+    angle = slider.value();
+    stroke(255);
+    translate(windowWidth/2,height);
+    branch(100,0);
+}
+
+function branch(len,color){
+  color += 10;
+  stroke(color,255,255);
+  line(0,0,0,-len);
+  translate(0,-len);
+  if(len > 4) {
+    push();
+    rotate(angle);
+    branch(len*0.67,color);
+    pop();
+    push();
+    rotate(-angle,color);
+    branch(len*0.67);
+    pop();
+  }
+  //line(0,0,0,-len*0.67);
+}
+
+function windowResized(){
+  resizeCanvas(windowWidth,windowHeight);
+}

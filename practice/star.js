2d4410911becc5fe9697510fe77770e5040892da
Whitbyfish <34402877+Whitbyfish@users.noreply.github.com>
2018-10-24 01:09:34 +0100

    a

--- practice/star.js
+++ practice/star.js
@@ -5,8 +5,8 @@ function Star(sx,sy,ex,ey,img){
   this.size = random(10,30);
   this.vel = p5.Vector.random2D();
   this.acc = createVector(0,0);
-  this.maxspeed = 20;
-  this.maxforce = 2;
+  this.maxspeed = 10;
+  this.maxforce = 1;
   this.settled = false;
 }
 

8e0fb21fc13cabbc7692a213d1ebf9368b6c353d
Whitbyfish <34402877+Whitbyfish@users.noreply.github.com>
2018-10-24 01:42:32 +0100

    a

--- practice/star.js
+++ practice/star.js
@@ -5,9 +5,11 @@ function Star(sx,sy,ex,ey,img){
   this.size = random(10,30);
   this.vel = createVector(random(-10,10),random(-10,10));
   this.acc = createVector(0,0);
+  this.angle = random(TWO_PI);
   this.maxspeed = 2;
   this.maxforce = 0.1;
   this.settled = false;
+  this.dir = (random(1) > 0.5) ? 1 : -1;
 }
 
 Star.prototype.checkSettled = function(){
@@ -20,6 +22,7 @@ Star.prototype.update = function(){
   this.pos.add(this.vel);
   this.vel.add(this.acc);
   this.acc.mult(0);
+  this.angle += this.dir * this.vel.mag() / 200;
   this.checkSettled();
 }
 
@@ -34,8 +37,11 @@ Star.prototype.applyForce = function(f){
 }
 
 Star.prototype.show = function(){
+  push();
   imageMode(CENTER);
+  rotate(this.angle);
   image(this.img,this.pos.x,this.pos.y,this.size,this.size);
+  pop();
 }
 
 Star.prototype.arrive = function(endpos){

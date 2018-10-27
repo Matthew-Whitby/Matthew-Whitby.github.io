9040136df511b71dc33918fe18c640611f310ee8
Whitbyfish <34402877+Whitbyfish@users.noreply.github.com>
2018-10-27 16:18:10 +0100

    a

--- practice/particletext/sketch.js
+++ practice/particletext/sketch.js
@@ -51,14 +51,6 @@ function keyPressed() {
     for(particle of particles){
       particle.setSettled(false);
     }
-  }else if(keyCode === 75){
-    var x = document.getElementsByTagName("Button");
-    for(var i = 0; i < x.length; i++){
-      console.log(x[i]);
-      x[i].style.visbility = "hidden";
-      console.log(x[i]);
-    }
-    
   }
 }
 

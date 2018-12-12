dcb0e05445689b343e191a484aad06aa8c88b358
Whitbyfish <matj1@aol.com>
2018-12-12 17:58:07 +0000

    Update sketch.js

--- practice/amplitude/sketch.js
+++ practice/amplitude/sketch.js
@@ -1,31 +1,17 @@
-var song;
-var button;
-var amp;
-var fft;
-var volhistory = [];
-var ampPath = false;
-var w;
+var song,button,amp,fft,volhistory=[],ampPath=false,w;
 
-function preload(){
-  song = loadSound('05 Again×Again.mp3');
-}
+function preload(){song=loadSound('05 Again×Again.mp3');}
 
-function toggleSong(){
-  if(song.isPlaying()) song.pause();
-  else song.play();
-}
+function toggleSong(){(song.isPlaying())?song.pause():song.play();}
 
-function toggleGraph(){
-  if(!ampPath) ampPath = true;
-  else ampPath = false;
-}
+function toggleGraph(){ampPath=(ampPath)?false:true;}
 
 function setup(){
-  let canvas = createCanvas(windowWidth,windowHeight);
+  let canvas=createCanvas(windowWidth,windowHeight);
   canvas.position(0,0);
   canvas.style('z-index','-1');
-  button = createButton('play/pause');
-  button2 = createButton('Change Graph');
+  button=createButton('play/pause');
+  button2=createButton('Change Graph');
   button2.position(100,0);
   button2.mousePressed(toggleGraph);
   colorMode(HSB);
@@ -33,27 +19,26 @@ function setup(){
   button.mousePressed(toggleSong);
   song.setVolume(0.4);
   song.play();
-  amp = new p5.Amplitude();
-  fft = new p5.FFT(0.85,128);
-  w = windowWidth / 90;
+  amp=new p5.Amplitude();
+  fft=new p5.FFT(0.85,128);
+  w=windowWidth/90;
 }
 
 function draw(){
   background(0);
-  let vol = amp.getLevel();
+  let vol=amp.getLevel();
   volhistory.push(vol);
-  if(ampPath) ampDraw();
-  else fftDraw();
+  (ampPath)?ampDraw():fftDraw();
 }
 
 function fftDraw(){
   noStroke();
-  let spectrum = fft.analyze();
-  for(let i = 0; i < spectrum.length;i++){
-    let amplit = spectrum[i];
+  let spectrum=fft.analyze();
+  for(let i=0;i<spectrum.length;i++){
+    let amplit=spectrum[i];
     fill(i,255,255);
-    let y = map(amplit,0,256,height,0);
-    rect(i*w,y,w -2,height-y);
+    let y=map(amplit,0,256,height,0);
+    rect(i*w,y,w-2,height-y);
   }
 }
 
@@ -61,39 +46,28 @@ function ampDraw(){
   stroke(255);
   noFill();
   beginShape();
-  for(var i = 0; i < volhistory.length;i++){
-    let y = map(volhistory[i],0,0.5,height/2,-height/2);
+  for(var i=0;i<volhistory.length;i++){
+    let y=map(volhistory[i],0,0.5,height/2,-height/2);
     vertex(i,y);
   }
   endShape();
   beginShape();
-  for(let i = 0; i < volhistory.length;i++){
-    let y = map(volhistory[i],0,0.5,height/2,height*1.5);
+  for(let i=0;i<volhistory.length;i++){
+    let y=map(volhistory[i],0,0.5,height/2,height*1.5);
     vertex(i,y);
   }
   endShape();
-  if(volhistory.length > width){
-    volhistory.splice(0,1);
-  }
+  if(volhistory.length>width)volhistory.splice(0,1);
 }
 
-function windowResized(){
-  resizeCanvas(windowWidth,windowHeight);
-}
+function windowResized(){resizeCanvas(windowWidth,windowHeight);}
 
 function keyPressed(){
-  if(keyCode === 72){
-    var x = document.getElementsByTagName("Button");
-    if(x.length > 0){
-       if(x[0].hidden == false){
-          for(var i = 0; i < x.length; i++){
-             x[i].hidden = true;
-           }
-       }else{
-          for(var i = 0; i < x.length; i++){
-             x[i].hidden = false;
-           }
-       }
+  if(keyCode===72){
+    var x=document.getElementsByTagName("Button");
+    if(x.length>0){
+       if(x[0].hidden==false)for(var i=0;i<x.length;i++)x[i].hidden=true;
+       else for(var i=0;i<x.length;i++)x[i].hidden=false;
     }
   }
 }

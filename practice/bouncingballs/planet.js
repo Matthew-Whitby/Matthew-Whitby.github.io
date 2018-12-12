e8e4bac934bd36b34765fbaf3ad3a08f7a3d3430
Whitbyfish <matj1@aol.com>
2018-12-12 18:04:28 +0000

    a

--- practice/bouncingballs/planet.js
+++ practice/bouncingballs/planet.js
@@ -1,37 +1,37 @@
-function Planet(m,x,y) {
-  this.mass = m;
-  this.pos = createVector(x,y);
-  this.velocity = createVector(0,0);
-  this.acceleration = createVector(0,0);
+function Planet(m,x,y){
+  this.mass=m;
+  this.pos=createVector(x,y);
+  this.velocity=createVector(0,0);
+  this.acceleration=createVector(0,0);
 }
 
-Planet.prototype.update = function() {
+Planet.prototype.update=()=>{
   this.velocity.add(this.acceleration);
   this.pos.add(this.velocity);
   this.acceleration.mult(0);
 }
 
-Planet.prototype.applyForce = function(force){
-  var f = p5.Vector.div(force,this.mass);
+Planet.prototype.applyForce=(force)=>{
+  var f=p5.Vector.div(force,this.mass);
   this.acceleration.add(f);
 }
 
-Planet.prototype.show = function(){
+Planet.prototype.show=()=>{
   stroke(255);
   strokeWeight(16*this.mass);
   point(this.pos.x,this.pos.y);
 }
 
-Planet.prototype.checkEdges = function(){
-  if(this.pos.x > width){
-    this.pos.x = width;
-    this.velocity.x *= -1;
-  }else if(this.pos.x < 0) {
-    this.velocity.x *= -1;
-    this.pos.x = 0;
+Planet.prototype.checkEdges=()=>{
+  if(this.pos.x>width){
+    this.pos.x=width;
+    this.velocity.x*=-1;
+  }else if(this.pos.x<0){
+    this.velocity.x*=-1;
+    this.pos.x=0;
   }
-  if(this.pos.y > height){
-    this.velocity.y *= -1;
-    this.pos.y = height;
+  if(this.pos.y>height){
+    this.velocity.y*=-1;
+    this.pos.y=height;
   }
 }
--- practice/bouncingballs/sketch.js
+++ practice/bouncingballs/sketch.js
@@ -1,19 +1,17 @@
-let planets = [];
+let planets=[];
 
 function setup(){
-  let canvas = createCanvas(windowWidth,windowHeight);
+  let canvas=createCanvas(windowWidth,windowHeight);
   canvas.position(0,0);
   canvas.style('z-index','-1');
-  for(var i = 0; i < 20; i++){
-    planets.push(new Planet(random(1,4),0,0));
-  }
+  for(var i=0;i<20;i++)planets.push(new Planet(random(1,4),0,0));
 }
 
 function draw(){
   background(20);
   for(planet of planets){
-    var wind = createVector(0.01,0);
-    var gravity = createVector(0,0.1*planet.mass);
+    var wind=createVector(0.01,0);
+    var gravity=createVector(0,0.1*planet.mass);
     planet.applyForce(wind);
     planet.applyForce(gravity);
     planet.update();
@@ -22,23 +20,14 @@ function draw(){
   }
 }
 
-function windowResized(){
-  resizeCanvas(windowWidth,windowHeight);
-}
+function windowResized(){resizeCanvas(windowWidth,windowHeight);}
 
 function keyPressed(){
-  if(keyCode === 72){
-    var x = document.getElementsByTagName("Button");
-    if(x.length > 0){
-       if(x[0].hidden == false){
-          for(var i = 0; i < x.length; i++){
-             x[i].hidden = true;
-           }
-       }else{
-          for(var i = 0; i < x.length; i++){
-             x[i].hidden = false;
-           }
-       }
+  if(keyCode===72){
+    var x=document.getElementsByTagName("Button");
+    if(x.length>0){
+       if(x[0].hidden==false)for(var i=0;i<x.length;i++)x[i].hidden=true;
+       else for(var i=0;i<x.length;i++)x[i].hidden=false;
     }
   }
 }

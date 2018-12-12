ce2613c9b451ae68cda3c724c3437b8173ec0a93
Whitbyfish <matj1@aol.com>
2018-12-12 18:20:27 +0000

    Update sketch.js

--- practice/fractaltree/sketch.js
+++ practice/fractaltree/sketch.js
@@ -1,29 +1,28 @@
-var angle = 0;
-var slider;
+var angle=0,slider;
 
 function setup(){
-  let canvas = createCanvas(windowWidth,windowHeight);
+  let canvas=createCanvas(windowWidth,windowHeight);
   canvas.position(0,0);
   canvas.style('z-index','-1');
-  slider = createSlider(0,TWO_PI,PI/4,0.01);
+  slider=createSlider(0,TWO_PI,PI/4,0.01);
   slider.position(0,0);
   colorMode(HSB);
 }
 
 function draw(){
     background(0);
-    angle = slider.value();
+    angle=slider.value();
     stroke(255);
     translate(windowWidth/2,height);
     branch(windowHeight/4,0);
 }
 
 function branch(len,color){
-  color += 10;
+  color+=10;
   stroke(color,255,255);
   line(0,0,0,-len);
   translate(0,-len);
-  if(len > 4) {
+  if(len>4){
     push();
     rotate(angle);
     branch(len*0.67,color);
@@ -35,23 +34,14 @@ function branch(len,color){
   }
 }
 
-function windowResized(){
-  resizeCanvas(windowWidth,windowHeight);
-}
+function windowResized(){resizeCanvas(windowWidth,windowHeight);}
 
 function keyPressed(){
-  if(keyCode === 72){
-    var x = document.getElementsByTagName("Button");
-    if(x.length > 0){
-       if(x[0].hidden == false){
-          for(var i = 0; i < x.length; i++){
-             x[i].hidden = true;
-           }
-       }else{
-          for(var i = 0; i < x.length; i++){
-             x[i].hidden = false;
-           }
-       }
+  if(keyCode===72){
+    var x=document.getElementsByTagName("Button");
+    if(x.length>0){
+       if(x[0].hidden==false)for(var i=0;i<x.length;i++)x[i].hidden=true;
+       else for(var i=0;i<x.length;i++)x[i].hidden=false;
     }
   }
 }

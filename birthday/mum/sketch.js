133a340572c48080e0a745a54a8eab2e37bcac08
Matthew Whitby <34402877+Whitbyfish@users.noreply.github.com>
2020-01-15 14:12:57 +0000

    Update sketch.js

--- birthday/mum/sketch.js
+++ birthday/mum/sketch.js
@@ -1,105 +1,98 @@
-var happyFireworks = [];
-var birthdayFireworks = [];
-var robertFireworks = [];
+var happyFireworks=[],birthdayFireworks=[],mumFireworks=[],fireworks=[];
 var font;
 var gravity;
-var stage = 0;
-var fireworks = [];
+var stage=0;
 
 function preload(){
-  font = loadFont('07にくまるフォント.otf');
+  font=loadFont('07にくまるフォント.otf');
 }
 
 function setup(){
-  let canvas = createCanvas(windowWidth,windowHeight);
+  let canvas=createCanvas(windowWidth,windowHeight);
   canvas.position(0,0);
   canvas.style('z-index','-1');
   colorMode(HSB);
-  gravity = createVector(0,0.1);
+  gravity=createVector(0,0.1);
   background(0);
   textFont(font);
   stroke(255);
   strokeWeight(4);
 
-  let happyPoints = font.textToPoints('Happy',100,200,192);
-  let birthdayPoints = font.textToPoints('Birthday',100,200,192);
-  let robertPoints = font.textToPoints('Mum',100,200,192);
+  let happyPoints=font.textToPoints('Happy',100,200,192);
+  let birthdayPoints=font.textToPoints('Birthday',100,200,192);
+  let mumPoints=font.textToPoints('Mum',100,200,192);
 
-  let happyPointsLength = counterplz(happyPoints);
-  let birthdayPointsLength = counterplz(birthdayPoints);
-  let robertPointsLength = counterplz(robertPoints);
+  let happyPointsLength=counterplz(happyPoints);
+  let birthdayPointsLength=counterplz(birthdayPoints);
+  let mumPointsLength=counterplz(mumPoints);
 
-  happyPoints = font.textToPoints('Happy',(windowWidth/2)-(happyPointsLength/2),200,192);
-  birthdayPoints = font.textToPoints('Birthday',(windowWidth/2)-(birthdayPointsLength/2),200,192);
-  robertPoints = font.textToPoints('Mum',(windowWidth/2)-(robertPointsLength/2),200,192);
+  happyPoints=font.textToPoints('Happy',(windowWidth/2)-(happyPointsLength/2),200,192);
+  birthdayPoints=font.textToPoints('Birthday',(windowWidth/2)-(birthdayPointsLength/2),200,192);
+  mumPoints=font.textToPoints('Mum',(windowWidth/2)-(mumPointsLength/2),200,192);
 
-  for(ipoint of happyPoints){
+  for(ipoint of happyPoints)
     happyFireworks.push(new Firework(ipoint.x,ipoint.y));
-  }
-  for(ipoint of birthdayPoints){
+  for(ipoint of birthdayPoints)
     birthdayFireworks.push(new Firework(ipoint.x,ipoint.y));
-  }
-  for(ipoint of robertPoints){
+  for(ipoint of mumPoints)
     robertFireworks.push(new Firework(ipoint.x,ipoint.y));
-  }
 }
 
 function counterplz(pointList){
-  let maxX = 0;
-  let minX = 0;
+  let maxX=0;
+  let minX=0;
   for(ipoint of pointList){
-    if(ipoint.x > maxX) maxX = ipoint.x;
-    else if(ipoint.x < minX) minX = ipoint.x;
+    if(ipoint.x>maxX)maxX=ipoint.x;
+    else if(ipoint.x<minX)minX=ipoint.x;
   }
-  let lengthOfText = maxX - minX;
-  return lengthOfText;
+  return maxX-minX;
 }
 
 function draw(){
   colorMode(RGB);
   background(0,0,0,25);
-  if(stage == 0) drawHappy();
-  else if(stage == 1) drawBirthday();
-  else if(stage == 2) drawRobert();
+  if(stage==0)drawHappy();
+  else if(stage==1)drawBirthday();
+  else if(stage==2)drawMum();
   else{
-    if(random(1) < 0.1) fireworks.push(new Firework(-1000,-1000));
-    for (let i = fireworks.length - 1; i >= 0; i--) {
+    if(random(1)<0.1)fireworks.push(new Firework(-1000,-1000));
+    for (let i=fireworks.length-1;i>=0;i--) {
       fireworks[i].update();
       fireworks[i].show();
-      if(fireworks[i].done()) fireworks.splice(i,1);
+      if(fireworks[i].done())fireworks.splice(i,1);
     }
   }
 }
 
 function drawHappy(){
-  for(let i = happyFireworks.length-1; i >= 0;i--){
+  for(let i=happyFireworks.length-1;i>=0;i--){
     happyFireworks[i].update();
     happyFireworks[i].show();
-    if(happyFireworks[i].done()) {
+    if(happyFireworks[i].done()){
       happyFireworks.splice(i,1);
-      if(happyFireworks.length == 0) stage = 1;
+      if(happyFireworks.length==0)stage=1;
     }
   }
 }
 
 function drawBirthday(){
-  for(let i = birthdayFireworks.length-1; i >= 0;i--){
+  for(let i=birthdayFireworks.length-1;i>=0;i--){
     birthdayFireworks[i].update();
     birthdayFireworks[i].show();
-    if(birthdayFireworks[i].done()) {
+    if(birthdayFireworks[i].done()){
       birthdayFireworks.splice(i,1);
-      if(birthdayFireworks.length == 0) stage = 2;
+      if(birthdayFireworks.length==0)stage=2;
     }
   }
 }
 
-function drawRobert(){
-  for(let i = robertFireworks.length-1; i >= 0;i--){
-    robertFireworks[i].update();
-    robertFireworks[i].show();
-    if(robertFireworks[i].done()) {
-      robertFireworks.splice(i,1);
-      if(robertFireworks.length == 0) stage = 3;
+function drawMum(){
+  for(let i=mumFireworks.length-1;i>=0;i--){
+    mumFireworks[i].update();
+    mumFireworks[i].show();
+    if(mumFireworks[i].done()){
+      mumFireworks.splice(i,1);
+      if(mumFireworks.length==0)stage=3;
     }
   }
 }
@@ -109,17 +102,15 @@ function windowResized(){
 }
 
 function keyPressed(){
-  if(keyCode === 72){
-    var x = document.getElementsByTagName("Button");
-    if(x.length > 0){
-       if(x[0].hidden == false){
-          for(var i = 0; i < x.length; i++){
-             x[i].hidden = true;
-           }
+  if(keyCode===72){
+    var x=document.getElementsByTagName("Button");
+    if(x.length>0){
+       if(x[0].hidden==false){
+          for(var i=0;i<x.length;i++)
+             x[i].hidden=true;
        }else{
-          for(var i = 0; i < x.length; i++){
-             x[i].hidden = false;
-           }
+          for(var i=0;i<x.length;i++)
+             x[i].hidden=false;
        }
     }
   }
